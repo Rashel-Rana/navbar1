5ee03c34322b6f45deac0a0a9b567b019230176e
Rashel Rana <rashel24.bd@gmail.com>
2022-08-12 21:49:20 +0600

    mY FIRST JS fILE Is added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("rashel").innerHTML= " RASHEL RANA WILL BE THE GREATEST WEB DEVELOPER IN THE WORLD INSALLAH.";
+
+alert("This is a testing Site");
+
+function myFunction() {
+   document.getElementById('demo').innerHTML = " Rashel Rana is the best web developer in the world.";
+}
